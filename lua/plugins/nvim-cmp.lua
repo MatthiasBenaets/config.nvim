6f46555f46afe733251cecc8c532c628afbdfa1d
MatthiasBenaets <matthias.benaets@gmail.com>
2025-05-13 11:14:02 +0200

    plugin: nvim-cmp

---
+++ lua/plugins/nvim-cmp.lua
@@ -0,0 +1,49 @@
+-- Code completion
+return {
+  "hrsh7th/nvim-cmp",
+  event = "InsertEnter",
+  dependencies = {
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "L3MON4D3/LuaSnip",
+    "saadparwaiz1/cmp_luasnip",
+    "rafamadriz/friendly-snippets",
+  },
+  config = function()
+    local cmp = require("cmp")
+    local luasnip = require("luasnip")
+
+    -- Load vscode snippets from dependencies
+    require("luasnip.loaders.from_vscode").lazy_load()
+
+    -- Configuration
+    cmp.setup({
+      -- Popup menu for options with preview
+      completion = {
+        completeopt = "menu,menuone,preview,noinsert",
+      },
+      -- Cmp interact with Luasnip
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
+      },
+      -- Keymaps
+      mapping = cmp.mapping.preset.insert({
+        ["<C-k>"] = cmp.mapping.select_prev_item(), -- Previous suggestion
+        ["<C-j>"] = cmp.mapping.select_next_item(), -- Next suggestion
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4), -- Scoll docs down
+        ["<C-f>"] = cmp.mapping.scroll_docs(4), -- Scroll docs up
+        ["<C-Space>"] = cmp.mapping.complete(), -- Show cmp suggestions
+        ["<C-e>"] = cmp.mapping.abort(), -- Close suggestions
+        ["<CR>"] = cmp.mapping.confirm({ select = false }), -- Confirm selected suggestion
+      }),
+      -- Suggestion sources
+      sources = cmp.config.sources({
+        { name = "luasnip" }, -- Snippets
+        { name = "buffer" }, -- Text in buffer
+        { name = "path" }, -- File path
+      }),
+    })
+  end,
+}
